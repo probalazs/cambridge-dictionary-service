ed7a431d2f42b8098fbb4e398687c02bed69f11c
probalazs <probalazs@nyenyere.eu>
2022-07-09 14:01:32 +0200

    build: fix entrypoint

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { getHtml } from './get-html';
 import axios from 'axios';
 import { configuration } from './configuration';
 
-http('mainHttp', (request, response) =>
+http('translateGET', (request, response) =>
   find({
     getHtml: getHtml({ get: axios.get, baseUrl: configuration.wordEndpoint }),
   })(request.query['q']! as string).then(response.json),
