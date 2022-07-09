1eea3a1a4ce8013f7060a68af97bcaad7dc24275
probalazs <probalazs@nyenyere.eu>
2022-07-09 13:33:49 +0200

    build: test function deploy

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import { http } from '@google-cloud/functions-framework';
-import { find } from './find';
-import { getHtml } from './get-html';
-import axios from 'axios';
-import { configuration } from './configuration';
+// import { http } from '@google-cloud/functions-framework';
+// import { find } from './find';
+// import { getHtml } from './get-html';
+// import axios from 'axios';
+// import { configuration } from './configuration';
 
-http('main', (request, response) =>
-  find({
-    getHtml: getHtml({ get: axios.get, baseUrl: configuration.wordEndpoint }),
-  })(request.query['q']! as string).then(response.json),
-);
+// http('main', (request, response) =>
+//   find({
+//     getHtml: getHtml({ get: axios.get, baseUrl: configuration.wordEndpoint }),
+//   })(request.query['q']! as string).then(response.json),
+// );
+
+import { http } from '@google-cloud/functions-framework';
+
+http('main', (_, response) => {
+  response.json({ message: 'Hellow Word!' });
+});
